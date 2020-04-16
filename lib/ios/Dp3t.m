a40daa92c7e45c7dd1e77d8f0ada12a4b448740d
Ran Greenberg <gran33@gmail.com>
2020-04-16 15:51:17 +0300

    make iOS work

--- lib/ios/Dp3t.m
+++ lib/ios/Dp3t.m
@@ -11,4 +11,36 @@ RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnu
     callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
 }
 
+RCT_EXPORT_METHOD(startScan) {
+    NSLog(@"TODO startScan");
+}
+
+RCT_EXPORT_METHOD(startAdvertising) {
+    NSLog(@"TODO startAdvertising");
+}
+
+RCT_EXPORT_METHOD(startScanningAndAdvertising) {
+    NSLog(@"TODO startScanningAndAdvertising");
+}
+
+RCT_EXPORT_METHOD(stop) {
+    NSLog(@"TODO stop");
+}
+
+RCT_EXPORT_METHOD(sync) {
+    NSLog(@"TODO sync");
+}
+
+RCT_EXPORT_METHOD(getScanInterval) {
+    NSLog(@"TODO getScanInterval");
+}
+
+RCT_EXPORT_METHOD(getHandshakes) {
+    NSLog(@"TODO getHandshakes");
+}
+
+RCT_EXPORT_METHOD(getStatus:(id)callback) {
+    NSLog(@"TODO getStatus ");
+}
+
 @end
--- react-native-dp3t.podspec
+++ react-native-dp3t.podspec
@@ -15,10 +15,10 @@ Pod::Spec.new do |s|
   # optional - use expanded license entry instead:
   # s.license    = { :type => "MIT", :file => "LICENSE" }
   s.authors      = { "Lev" => "yourname@email.com" }
-  s.platforms    = { :ios => "9.0" }
+  s.platforms    = { :ios => "10.0" }
   s.source       = { :git => "https://github.com/github_account/react-native-dp3t.git", :tag => "#{s.version}" }
 
-  s.source_files = "ios/**/*.{h,m,swift}"
+  s.source_files = "lib/ios/**/*.{h,m,swift}"
   s.requires_arc = true
 
   s.dependency "React"
